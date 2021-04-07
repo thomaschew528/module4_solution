04c956c2983f9e8e32454cee91492acd51e76f11
Keat Wah Chew <keatwahchew@Keats-MBP.lan>
2021-04-07 10:23:03 +0800

    update

--- easier/SpeakGoodBye.js
+++ easier/SpeakGoodBye.js
@@ -11,7 +11,7 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+//var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
@@ -23,3 +23,13 @@ function speak(name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+
+(function(window){
+  var byeSpeaker = {};
+  byeSpeaker.speak = function (name){
+  var speakWord = "Good Bye";
+  console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
+
